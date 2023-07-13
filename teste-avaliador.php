3a57863695bfede5776f4fa4fef05588e787e09c
maucaruso <carusojr@outlook.com>
2023-07-13 08:23:21 -0300

    chore: adding comments to the manual test

--- teste-avaliador.php
+++ teste-avaliador.php
@@ -7,6 +7,7 @@ use Alura\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
+// Arrumo a casa para o teste / Arrange / Given
 $leilao = new Leilao('Renault Clio 2007');
 
 $maria = new Usuario('Maria');
@@ -16,8 +17,17 @@ $leilao->recebeLance(new Lance($joao, 2000));
 $leilao->recebeLance(new Lance($maria, 2500));
 
 $leiloeiro = new Avaliador();
+
+// Executa o código a ser testado / Act / When
 $leiloeiro->avalia($leilao);
 
 $maiorValor = $leiloeiro->getMaiorValor();
 
-echo $maiorValor;
+// Verifico se a saída é a esperada / Assert / Then
+$valorEsperado = 2500;
+
+if ($valorEsperado == $maiorValor) {
+  echo 'TESTE OK';
+} else {
+  echo 'TESTE FALHOU';
+}
