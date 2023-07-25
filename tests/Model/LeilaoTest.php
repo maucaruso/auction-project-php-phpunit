cba88d70b9fe07ddbb99790c42acb3bf8a8eca88
maucaruso <carusojr@outlook.com>
2023-07-25 07:21:11 -0300

    test: LeilaoTest

--- tests/Model/LeilaoTest.php
+++ tests/Model/LeilaoTest.php
@@ -22,6 +22,18 @@ class LeilaoTest extends TestCase
     }
   }
   
+  public function testLeilaoNaoDeveReceberLancesRepetidos()
+  {
+    $leilao = new Leilao('Variante');
+    $ana = new Usuario('Ana');
+    
+    $leilao->recebeLance(new Lance($ana, 1000));
+    $leilao->recebeLance(new Lance($ana, 1500));
+    
+    static::assertCount(1, $leilao->getLances());
+    static::assertEquals(1000, $leilao->getLances()[0]->getValor());
+  }
+  
   public function geraLances()
   {
     $joao = new Usuario('João');
@@ -33,6 +45,8 @@ class LeilaoTest extends TestCase
     
     $leilaoCom1Lance = new Leilao('Fusca 1972 0KM');
     $leilaoCom1Lance->recebeLance(new Lance($maria, 5000));
+    
+    
     
     return [
       '2-lances' => [2, $leilaoCom2Lances, [1000, 2000]],
